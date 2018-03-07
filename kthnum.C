5436c6b8c690c9b8049aa180f1734e662648493b
pattumehala <35220817+pattumehala@users.noreply.github.com>
2018-03-07 15:58:16 +0530

    kthnum.C
    print the kth number

---
+++ kthnum.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int n,k,a[10],i;
+printf("\nEnter the n number:");
+scanf("%d",&n);
+printf("\nEnter the kth numbers:")
+scanf("%d",&k);
+for(i=0;i<n;i++);
+{
+scanf("%d\t",&a[i]);
+}
+printf("%d",a[k-1]);
+return 0;
+}
